8782e4f162cfc68e43b2c2675e762e4b040d7ae8
gtgtKim <kgtrlarbxo@gmail.com>
2024-08-28 14:24:25 +0900

    cdn added

--- app.js
+++ app.js
@@ -53,6 +53,11 @@ app.get("/product-detail/:id", (req, res) => {
   res.render("product-detail", { productId: productId });
 });
 
+app.get("/events/:id", (req, res) => {
+  const eventId = req.params.id;
+  res.render("events", { eventId: eventId });
+});
+
 // 404 Error handler
 app.use((req, res, next) => {
   res.status(404).render("404", { url: req.originalUrl });
