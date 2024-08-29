38238e4ee5149d44ed06e2df57be6ff0c2a1d425
gtgtKim <kgtrlarbxo@gmail.com>
2024-08-29 18:21:12 +0900

    test11

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const dotenv = require("dotenv");
 dotenv.config();
+
+console.log("Loaded environment variables:", {
+  DB_HOST: process.env.DB_HOST,
+  DB_USER: process.env.DB_USER,
+  DB_PASS: process.env.DB_PASS,
+  DB_NAME: process.env.DB_NAME,
+  SESSION_SECRET: process.env.SESSION_SECRET,
+});
 const db = require("./db");
 const express = require("express");
 const path = require("path");
